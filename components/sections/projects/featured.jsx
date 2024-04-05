08b30a6228f604c3b16f7780a2cbf3bf66eb4fe8
gnzv1 <162704158+gnzv1@users.noreply.github.com>
2024-04-05 13:10:42 -0300

    Update featured.jsx

--- components/sections/projects/featured.jsx
+++ components/sections/projects/featured.jsx
@@ -4,7 +4,6 @@ import FeaturedProject from '../../blocks/projects/featured'
 // Section structure
 import Section 		from '../../structure/section';
 import Container 	from '../../structure/container';
-import Badges 		from '../../utils/badge.list.util'
 import SectionTitle from '../../blocks/section.title.block'
 
 import css 			from '../../../styles/sections/projects/featured.module.scss'
